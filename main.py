3d555656d5303696696fb098e84f55ebaff57a89
giovannioddo90 <giovannioddo90@gmail.com>
2025-05-16 19:49:06 -0400

    add sys module for book path cli input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, char_count, sort_dict
 
 def get_book_text(book_path):
@@ -5,7 +6,11 @@ def get_book_text(book_path):
         return f.read()
 
 def main():
-    book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book = sys.argv[1]
     text = get_book_text(book)
     word_count = count_words(text)
     chars_dict = char_count(text)
